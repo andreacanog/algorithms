1dc1179b1351eaf7f380a241452fae8dc63f6d26
Andrea Cano <andreacanogisbert@gmail.com>
2023-09-13 11:16:26 -0700

    LC 128 Longest Consecutive Sequence python - LC 75

--- Leetcode75/ArraysAndHashing/LC-128-longest_consecutive_sequence.py
+++ Leetcode75/ArraysAndHashing/LC-128-longest_consecutive_sequence.py
@@ -14,3 +14,23 @@
 # Input: nums = [0,3,7,2,5,8,4,6,0,1]
 # Output: 9
 
+## time complexity: O(n)
+## space complexity: O(n)
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for num in nums:
+            if (num - 1) not in numSet:
+                length = 0
+
+                while (num + length) in numSet:
+                    length += 1
+                
+                longest = max(longest, length)
+        
+        return longest
+
+
