7db48ce9dcf76255090e5f47db80b215de75221b
Andrea Cano <andreacanogisbert@gmail.com>
2023-09-13 15:18:44 -0700

    LC 125 Valid Palindrome python - LC 75

--- Leetcode75/TwoPointers/LC-125-valid_palindrome.py
+++ Leetcode75/TwoPointers/LC-125-valid_palindrome.py
@@ -20,3 +20,30 @@
 # Explanation: s is an empty string "" after removing non-alphanumeric characters.
 # Since an empty string reads the same forward and backward, it is a palindrome.
 
+## Time: O(N) | Space: O(1)
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while left < right and not self.isAlphanumeric(s[left]):
+                left += 1
+            while left < right and not self.isAlphanumeric(s[right]):
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+
+        return True 
+    
+
+    def isAlphanumeric(self, char):
+        return (
+            (ord('A') <= ord(char) <= ord('Z')) or
+            (ord('a') <= ord(char) <= ord('z')) or
+            (ord('0') <= ord(char) < ord('9'))
+        )
