3cae07f82dc68e2a538b4e5e73598a8e809cb2f3
Andrea Cano <andreacanogisbert@gmail.com>
2023-09-18 15:34:01 -0700

    LC 207 Course Schedule Javascript - LC 75

--- Leetcode75/Graphs/LC-207_course_schedule.py
+++ Leetcode75/Graphs/LC-207_course_schedule.py
@@ -18,3 +18,38 @@
 # Output: false
 # Explanation: There are a total of 2 courses to take. 
 # To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+
+var canFinish = function(numCourses, prerequisites) {
+    let coursesGraph = new Map();
+    let visiting = new Set();
+
+    for (let i = 0; i < numCourses; i++) {
+        coursesGraph.set(i, []);
+    }
+
+    for (let [course, pre] of prerequisites) {
+        coursesGraph.get(course).push(pre)
+    }
+
+    const dfs = course => {
+        if (visiting.has(course)) return false;
+        if (!coursesGraph.get(course).length) return true;
+
+        visiting.add(course)
+
+        for (let pre of coursesGraph.get(course)) {
+            if (!dfs(pre)) return false;
+        }
+
+        visiting.delete(course);
+        coursesGraph.set(course, [])
+        return true;
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+        if (!dfs(i)) return false;
+    }
+
+    return true;
+}
