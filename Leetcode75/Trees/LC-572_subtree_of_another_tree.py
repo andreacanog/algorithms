12382650357593fd4e05fbf07f26e8e71ba42597
Andrea Cano <andreacanogisbert@gmail.com>
2023-09-19 11:38:18 -0700

    LC 572 Subtree of Another Tree Javascript - LC 75

--- Leetcode75/Trees/LC-572_subtree_of_another_tree.py
+++ Leetcode75/Trees/LC-572_subtree_of_another_tree.py
@@ -16,3 +16,33 @@
 
 # Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
 # Output: false
+
+# /**
+#  * Definition for a binary tree node.
+#  * function TreeNode(val, left, right) {
+#  *     this.val = (val===undefined ? 0 : val)
+#  *     this.left = (left===undefined ? null : left)
+#  *     this.right = (right===undefined ? null : right)
+#  * }
+#  */
+# /**
+#  * @param {TreeNode} root
+#  * @param {TreeNode} subRoot
+#  * @return {boolean}
+#  */
+var isSubtree = function(root, subRoot) {
+    if (!subRoot) return true;
+    if (!root) return false;
+
+    if (sameTree(root, subRoot)) return true;
+
+    return (isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot))
+    
+};
+
+var sameTree = function(a, b) {
+    if (!a && !b) return true;
+    if ((!a || !b) || a.val !== b.val) return false;
+
+    return (sameTree(a.left, b.left) && sameTree(a.right, b.right))
+}
