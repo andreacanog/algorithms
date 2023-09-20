ef7d13014fc06d9e99118c874fbbdd91ea77e617
Andrea Cano <andreacanogisbert@gmail.com>
2023-09-20 13:42:16 -0700

    LC 105 Construct Binary Tree from Preorder and Inorder Traversal Javascript - LC 75

---
+++ Leetcode75/Trees/LC-105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -0,0 +1,30 @@
+# Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder 
+# is the inorder traversal of the same tree, construct and return the binary tree.
+
+# Example 1:
+
+
+# Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+# Output: [3,9,20,null,null,15,7]
+# Example 2:
+
+# Input: preorder = [-1], inorder = [-1]
+# Output: [-1]
+
+# /**
+#  * Definition for a binary tree node.
+#  * function TreeNode(val, left, right) {
+#  *     this.val = (val===undefined ? 0 : val)
+#  *     this.left = (left===undefined ? null : left)
+#  *     this.right = (right===undefined ? null : right)
+#  * }
+#  */
+# /**
+#  * @param {number[]} preorder
+#  * @param {number[]} inorder
+#  * @return {TreeNode}
+#  */
+
+var buildTree = function(preorder, inorder) {
+    
+};
--- Leetcode75/Trees/LC-230_kth_smallest_element_in_a_BST.py
+++ Leetcode75/Trees/LC-230_kth_smallest_element_in_a_BST.py
@@ -12,3 +12,38 @@
 
 # Input: root = [5,3,6,2,4,null,null,1], k = 3
 # Output: 3
+
+# /**
+#  * Definition for a binary tree node.
+#  * function TreeNode(val, left, right) {
+#  *     this.val = (val===undefined ? 0 : val)
+#  *     this.left = (left===undefined ? null : left)
+#  *     this.right = (right===undefined ? null : right)
+#  * }
+#  */
+# /**
+#  * @param {TreeNode} root
+#  * @param {number} k
+#  * @return {number}
+#  */
+
+var kthSmallest = function(root, k) {
+    let stack = [];
+    let curr = root;
+    let n = 0; 
+
+    while (stack.length || curr) {
+        while (curr) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+
+        curr = stack.pop();
+        n += 1;
+
+        if (n === k) return curr.val;
+
+        curr = curr.right
+    }
+
+}
