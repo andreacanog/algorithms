962c5a1f95c5bf35b21a43ca4cd92e9a6f6c7ec5
Andrea Cano <andreacanogisbert@gmail.com>
2023-10-04 15:38:53 -0700

    LC 15 3 Sum - LC 75

--- Leetcode75/TwoPointers/LC-15-3_sum.py
+++ Leetcode75/TwoPointers/LC-15-3_sum.py
@@ -57,3 +57,34 @@ class Solution:
 #  * @param {number[]} nums
 #  * @return {number[][]}
 #  */
+var threeSum = function(nums) {
+    let res = [];
+    nums = nums.sort((a,b) => a - b)
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let l = i + 1;
+        let r = nums.length - 1;
+
+        while (l < r) {
+            let sum = nums[i] + nums[l] + nums[r];
+
+            if (sum === 0) {
+                res.push([nums[i], nums[l], nums[r]])
+
+                while (nums[l] === nums[l + 1]) l++
+                while (nums[r] === nums[r - 1]) r--
+
+                l++
+                r--
+            } else if (sum > 0) {
+                r--
+            } else {
+                l++
+            }
+        }
+    }
+
+    return res;
+}
