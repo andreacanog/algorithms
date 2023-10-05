d0d4498e326a52621804e62165a931a624582fc0
Andrea Cano <andreacanogisbert@gmail.com>
2023-10-04 17:11:39 -0700

    LC 11 Container With Most Water python - LC 75

--- Leetcode75/TwoPointers/LC-11_container_with_most_water.py
+++ Leetcode75/TwoPointers/LC-11_container_with_most_water.py
@@ -18,3 +18,20 @@
 # Input: height = [1,1]
 # Output: 1
 
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
+
+        l, r = 0, len(height) - 1
+
+        while l < r:
+            area = (r - l) * min(height[l], height[r])
+            res = max(res, area)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        
+        return res
+
