56b66ef931dc510df19d9c98881feceeaf535ecc
Andrea Cano <andreacanogisbert@gmail.com>
2023-10-26 15:33:18 -0700

    LC 3 Longest Substring Without Repeating Characters JavaScript - LC 75

--- Leetcode75/SlidingWindow/LC-3_longest_substring_without_repeating_characters.py
+++ Leetcode75/SlidingWindow/LC-3_longest_substring_without_repeating_characters.py
@@ -41,3 +41,26 @@ class Solution:
             charSet.add(s[right])
             res = max(res, right - left + 1)
         return res
+    
+
+
+# /**
+#  * @param {string} s
+#  * @return {number}
+#  */
+# var lengthOfLongestSubstring = function(s) {
+#     let charSet = new Set();
+#     let left = 0;
+#     let res = 0; 
+
+#     for (let right = 0; right < s.length; right++) {
+#         while (charSet.has(s[right])) {
+#             charSet.delete(s[left]);
+#             left += 1
+#         }
+#         charSet.add(s[right])
+#         res = Math.max(res, right - left + 1)
+#     } 
+
+#     return res;    
+# };
