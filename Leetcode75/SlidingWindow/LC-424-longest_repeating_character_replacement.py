f426a4f55f178a0d9a8ee81235fc2e12f5a6652b
Andrea Cano <andreacanogisbert@gmail.com>
2023-10-27 10:23:46 -0700

    LC 424 Longest Repeating Character Replacement python - LC 75

--- Leetcode75/SlidingWindow/LC-424-longest_repeating_character_replacement.py
+++ Leetcode75/SlidingWindow/LC-424-longest_repeating_character_replacement.py
@@ -24,3 +24,22 @@
 # 1 <= s.length <= 105
 # s consists of only uppercase English letters.
 # 0 <= k <= s.length
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        left = 0
+        res = 0
+        maxS = 0
+
+        for right in range(len(s)):
+            count[s[right]] = 1 + count.get(s[right], 0)
+            maxS = max(maxS, count[s[right]])
+
+            while (right - left + 1) - maxS > k:
+                count[s[left]] -= 1
+                left += 1
+            res = max(res, right - left + 1)
+        
+        return res
