579bbc0680fec607bb2f8937595942efbbc249f3
Andrea Cano <andreacanogisbert@gmail.com>
2023-10-27 14:25:58 -0700

    LC 424 Longest Repeating Character Replacement JavaScript - LC 75

--- Leetcode75/SlidingWindow/LC-424-longest_repeating_character_replacement.py
+++ Leetcode75/SlidingWindow/LC-424-longest_repeating_character_replacement.py
@@ -43,3 +43,34 @@ class Solution:
             res = max(res, right - left + 1)
         
         return res
+
+
+
+# /**
+#  * @param {string} s
+#  * @param {number} k
+#  * @return {number}
+#  */
+# var characterReplacement = function(s, k) {
+#     let count = {};
+#     let left = 0;
+#     let res = 0;
+#     let maxS = 0;
+
+#     for (let right = 0; right < s.length; right++) {
+#         if (!count[s[right]]) {
+#             count[s[right]] = 0
+#         }
+#         count[s[right]] += 1
+#         maxS = Math.max(maxS, count[s[right]])
+
+#         while ((right - left + 1) - maxS > k) {
+#             count[s[left]] -= 1;
+#             left++
+#         }
+
+#         res = Math.max(res, right - left + 1)
+#     }
+#         return res;
+    
+# };
