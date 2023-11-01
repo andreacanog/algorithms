ad4781ea8c8af0a89cf92f5a6dfde64ffcd4a473
Andrea Cano <andreacanogisbert@gmail.com>
2023-11-01 15:50:08 -0700

    LC 76 Minimum Window Substring Python - LC 75

--- Leetcode75/SlidingWindow/LC-76_minimum_window_substring.py
+++ Leetcode75/SlidingWindow/LC-76_minimum_window_substring.py
@@ -31,3 +31,37 @@
 # n == t.length
 # 1 <= m, n <= 105
 # s and t consist of uppercase and lowercase English letters.
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        res = 0
+        left = 0
+        window = defaultdict(int)
+        countT = defaultdict(int)
+
+        for char in t:
+            countT[char] += 1
+        have, need = 0, len(countT)
+        res, resLen = [-1,-1], float("infinity")
+        for right in range(len(s)):
+            char = s[right]
+            window[char] += 1
+
+            if char in countT and window[char] == countT[char]:
+                have += 1
+
+                while have == need:
+                    #update values
+                    if (right - left + 1) < resLen:
+                        res = [left, right]
+                        resLen = right - left + 1
+                    #pop from left
+                    window[s[left]] -= 1
+
+                    if s[left] in countT and window[s[left]] < countT[s[left]]:
+                        have -= 1
+                    
+                    left += 1
+        left, right = res
+        return s[left:right + 1] if resLen != float("infinity") else ""
